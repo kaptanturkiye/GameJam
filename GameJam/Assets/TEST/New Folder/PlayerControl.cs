c6d3736c01ff5104dab13eb6e301f17352923e5d
EfendiKilic <113355785+EfendiKilic@users.noreply.github.com>
2022-09-16 01:39:04 +0300

    movement
    movement

---
+++ GameJam/Assets/TEST/New Folder/PlayerControl.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    public CharacterController PlayerControler;
+    
+    private float speed = 6f;
+    private float TurnSmoothVelocity;
+    private float TurnSmoothTime = 0.1f;
+
+    public float GravityScale = -9.81f;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    public float JumpPower = 3f;    
+
+    private Vector3 Velcity;
+    private bool isGrounded;
+    private bool DoubleJump = false;
+    public float DoubleJumpPower = 5f;
+    private float basictimer = 0.01f;
+
+    private int doubleJumpCount = 0;
+    public Vector3 moveDir;
+
+    private Collider test;
+    
+    private void Awake()
+    {
+        PlayerControler = this.gameObject.GetComponent<CharacterController>();
+        test = this.gameObject.GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        Gravity();
+        PlayerMovement();
+        Jumping();
+        DoubleJumping();
+    }
+    
+
+    private void Jumping()
+    {
+        if (Input.GetButtonDown("Jump") && doubleJumpCount ==0)
+        {
+            if (isGrounded == true)
+            {
+                Velcity.y = Mathf.Sqrt(JumpPower * -2f * GravityScale); 
+                DoubleJump = true;
+                doubleJumpCount++;
+            }
+        }
+    }
+    private void DoubleJumping()
+    {
+        if (Input.GetButtonDown("Jump") && DoubleJump && doubleJumpCount ==1 && !isGrounded)
+        {
+            Velcity.y = Mathf.Sqrt(DoubleJumpPower * -2f * GravityScale); ;
+            DoubleJump = false;
+        }
+    }
+    private void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (isGrounded == true && Velcity.y < 0)
+        {
+            Velcity.y = -2f;
+            doubleJumpCount = 0;
+        }
+        
+        Velcity.y += GravityScale * Time.deltaTime;
+        PlayerControler.Move(Velcity * Time.deltaTime);
+    }
+    
+    public void PlayerMovement()
+    {
+        float HorziontalAxes = Input.GetAxisRaw("Horizontal");
+        float VerticalAxes = Input.GetAxisRaw("Vertical");
+        Vector3 MoveDirection = new Vector3(HorziontalAxes, 0f, VerticalAxes).normalized;
+
+        if (MoveDirection.magnitude >= 0.1f)
+        {
+            float TargetAngle = Mathf.Atan2(MoveDirection.x ,MoveDirection.z) * Mathf.Rad2Deg;
+            float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref TurnSmoothVelocity,
+                TurnSmoothTime);
+            
+            moveDir = Quaternion.Euler(0, TargetAngle, 0) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f,Angle,0f);
+            PlayerControler.Move(MoveDirection * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            this.gameObject.transform.Translate(other.gameObject.transform.position.x,
+                other.gameObject.transform.position.y + 2, other.gameObject.transform.position.z);
+        }
+    }
+}
---
+++ GameJam/Assets/TEST/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+    
+    private CharacterController playerControler;
+    private Vector3 velocity;
+    
+    private int doubleJumpCount = 0;
+    
+    private float speed = 12f;
+    private float gravity = -49.05f;
+    private float jumpHeight = 3.75f;
+    private float groundDistance = 0.4f;
+    
+    private bool isGrounded;
+    private bool doubleJump = false;
+    
+    private void Awake()
+    {
+        playerControler = this.gameObject.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Gravity();
+        Movement();
+        Jumping();
+        DoubleJumping();
+    }
+
+    private void Jumping()
+    {
+        if (Input.GetButtonDown("Jump") && doubleJumpCount == 0)
+        {
+            if (isGrounded == true)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                doubleJump = true;
+                doubleJumpCount++;
+            }
+        }
+    }
+
+    private void DoubleJumping()
+    {
+        if (Input.GetButtonDown("Jump") && doubleJumpCount == 1)
+        {
+            if (isGrounded == false)
+            { 
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                doubleJump = false;
+            }
+        }
+    }
+    
+    private void Movement()
+    {
+        float HorizontalAxes = Input.GetAxis("Horizontal");
+        float VerticalAxes = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * HorizontalAxes + transform.forward * VerticalAxes;
+        playerControler.Move(move * speed * Time.deltaTime);
+    }
+    
+    private void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded == true && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            doubleJumpCount = 0;
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        playerControler.Move(velocity * Time.deltaTime);
+        
+    }
+}
