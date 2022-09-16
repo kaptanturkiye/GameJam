64556057dcd4b4077e4ec709fe27a81611aca6bf
EfendiKilic <113355785+EfendiKilic@users.noreply.github.com>
2022-09-16 14:58:59 +0300

    lithing
    lithing

--- GameJam/Assets/TEST/PlayerMovement.cs
+++ GameJam/Assets/TEST/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -20,12 +21,20 @@ public class PlayerMovement : MonoBehaviour
     
     private bool isGrounded;
     private bool doubleJump = false;
-
-   
-
+    
+    [SerializeField] private Light torch;
+    private float torchTimer = 5f;
+    private float coolDownTimer = 5f;
+    private bool coolDownCheck = false;
+    
     private void Awake()
     {
         playerControler = this.gameObject.GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        torch.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -34,8 +43,63 @@ public class PlayerMovement : MonoBehaviour
         Movement();
         Jumping();
         DoubleJumping();
+        Lithing();
     }
 
+
+    private void Lithing()
+    {
+        
+        if (Input.GetKey(KeyCode.Q) && torchTimer >= 0 && coolDownCheck == false)
+        {
+            torch.gameObject.SetActive(true);
+                    
+            if (torch.gameObject.activeInHierarchy == true)
+            {
+                torchTimer -= Time.deltaTime;
+                // Debug.Log("IŞIK AÇIK" + (int)torchTimer);
+            }
+        }
+        
+        else if (torchTimer < 0)
+        {
+            torch.gameObject.SetActive(false);
+            coolDownCheck = true;
+
+            if (coolDownCheck == true && coolDownTimer >= 0)
+            {
+                coolDownTimer -= Time.deltaTime;
+                // Debug.Log("COOLDOWN" + (int)coolDownTimer);
+            }
+
+            if (coolDownTimer < 0)
+            {
+                torchTimer = 5f;
+                coolDownCheck = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q) && torch.gameObject.activeInHierarchy == true)
+        {
+            torch.gameObject.SetActive(false);
+            torchTimer = 5f;
+        }
+
+
+        #region  IŞIK RENGİ
+        
+        if (torch.gameObject.activeInHierarchy == true && Input.GetKey(KeyCode.Tab))
+        {
+            torch.color = Color.cyan;
+        }
+        else if (torch.gameObject.activeInHierarchy == true && Input.GetKeyUp(KeyCode.Tab))
+        {
+            torch.color = Color.red;
+        }
+        
+        #endregion
+    }
+    
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "kup")
@@ -44,7 +108,7 @@ public class PlayerMovement : MonoBehaviour
             if (other.gameObject.name == "kup" && Input.GetKeyDown(KeyCode.E))
             {
                 //Destroy(other.gameObject);
-                //other.gameObject.GetComponent<Transform>().localScale = new Vector3(20f, 20f, 20f);
+                other.gameObject.GetComponent<Transform>().localScale = new Vector3(20f, 20f, 20f);
             }
         }
     }
